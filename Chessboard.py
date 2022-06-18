26d60d0adf80a4635c77cf047c545a7cc444664d
Saurabh Kovoor <saurabhkovoor@gmail.com>
2022-06-18 18:32:44 +0800

    commit1

--- Chessboard.py
+++ Chessboard.py
@@ -157,3 +157,4 @@ class Chessboard:
         # Displays to user final output, which is number of solutions/goal states and total number of actions
         print("\n    All " + str(noOfSolutions) + " solutions were found.")
         print("    Total actions: " + str(Chessboard.actions))
+        print()
